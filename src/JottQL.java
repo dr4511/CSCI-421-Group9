c407c9cc4c0c4a99a5abbd0016ab38fa0c2664eb
abo3854 <124399186+abo3854@users.noreply.github.com>
2026-02-05 10:16:16 -0500

    Update JottQL.java
    Fixed indentation

--- src/JottQL.java
+++ src/JottQL.java
@@ -1,53 +1,53 @@
 import java.util.Scanner;
 
 public class JottQL {
-        public static void main(String[] args) {
-        System.out.println("Starting Database . . .");
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-                    System.out.print("Enter Command > ");
-                    String command = scanner.nextLine().trim();
-                    switch (command.toUpperCase()) {
-                        case "CREATE":
-                            // CREATE TABLE <tableName> (CRE
-                            // <attr> <type> <constraints>,
-                            // ...
-                            // );
-                            break;
+    public static void main(String[] args) {
+    System.out.println("Starting Database . . .");
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+            System.out.print("Enter Command > ");
+            String command = scanner.nextLine().trim();
+            switch (command.toUpperCase()) {
+                case "CREATE":
+                    // CREATE TABLE <tableName> (CRE
+                    // <attr> <type> <constraints>,
+                    // ...
+                    // );
+                    break;
 
-                        case "SELECT":
-                            // SELECT * FROM <tableName>;
-                            break;
+                case "SELECT":
+                    // SELECT * FROM <tableName>;
+                    break;
 
-                        case "INSERT":
-                            // INSERT <tableName> VALUES ( <row1>, <row2>, ... );
-                            break;
+                case "INSERT":
+                    // INSERT <tableName> VALUES ( <row1>, <row2>, ... );
+                    break;
 
-                        case "DROP":
-                            // DROP TABLE <tableName>;
-                            break;
+                case "DROP":
+                    // DROP TABLE <tableName>;
+                    break;
 
-                        case "ALTER":
-                            // ALTER TABLE <tableName> ADD <attrName> <type>;
-                            // ALTER TABLE <tableName> ADD <attrName> <type> DEFAULT <value>;
-                            // ALTER TABLE <tableName> ADD <attrName> <type> NOTNULL DEFAULT <value>;
-                            // ALTER TABLE <tableName> DROP <attrName>;
-                            break;
+                case "ALTER":
+                    // ALTER TABLE <tableName> ADD <attrName> <type>;
+                    // ALTER TABLE <tableName> ADD <attrName> <type> DEFAULT <value>;
+                    // ALTER TABLE <tableName> ADD <attrName> <type> NOTNULL DEFAULT <value>;
+                    // ALTER TABLE <tableName> DROP <attrName>;
+                    break;
 
-                        case "java":
-                            //java JottQL <dbLocation> <pageSize> <bufferSize> <indexing>
-                            break;
+                case "java":
+                    //java JottQL <dbLocation> <pageSize> <bufferSize> <indexing>
+                    break;
 
-                        case "<QUIT>":
-                            System.out.println("Exiting JottQL...");
-                            scanner.close();
-                            return;
+                case "<QUIT>":
+                    System.out.println("Exiting JottQL...");
+                    scanner.close();
+                    return;
 
-                        default:
-                            System.out.println("Unknown command.");
-                            break;
-                    }
+                default:
+                    System.out.println("Unknown command.");
+                    break;
             }
         }
+    }
 }
 
